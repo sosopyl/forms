bb79d832786b4b1912b82aeb00ba06572787cda6
sosopyl <122552117+sosopyl@users.noreply.github.com>
2023-01-23 15:50:48 +0300

    Create forma.js

---
+++ forma.js
@@ -0,0 +1,90 @@
+// 274
+// 1
+
+// let textarea = document.querySelector('textarea')
+// let text = document.querySelector('p')
+
+// textarea.addEventListener('blur', function()
+// {
+//     text.textContent = textarea.value
+// })
+
+// 275
+// 1
+
+// let btn = document.querySelector('#btn')
+// let inp = document.querySelector('#inp')
+// btn.addEventListener('click', function(){
+//     inp.disabled = true
+// })
+
+// 2
+
+// let btn1 = document.querySelector('#btn1')
+// let btn2 = document.querySelector('#btn2')
+// let inp = document.querySelector('#inp')
+// btn1.addEventListener('click', function(){
+//     inp.disabled = true
+// })
+// btn2.addEventListener('click', function(){
+//     inp.disabled = false
+// })
+
+// 3
+
+// let btn = document.querySelector('#btn')
+// let inp = document.querySelector('#inp')
+// btn.addEventListener('click', function(){
+//     if(inp.disabled == true ){console.log(true)
+//     } else{console.log(false)}
+// })
+
+// 276
+
+//1
+// let btn1 = document.querySelector('#btn1')
+// let btn2 = document.querySelector('#btn2')
+// let checkbox = document.querySelector('#checkbox')
+// btn1.addEventListener('click', function(){
+//     checkbox.checked = true
+// })
+// btn2.addEventListener('click', function(){
+//     checkbox.checked = false
+// })
+
+//2
+
+// let checkbox = document.querySelector('#checkbox')
+// let btn = document.querySelector('#btn')
+// let p = document.querySelector('#p')
+// btn.addEventListener('click', function(){
+//     if(checkbox.checked == true){p.textContent = 'привет'} else {p.textContent = 'пока' }
+// })
+
+// 277
+
+// button.addEventListener('click', function() {
+// 	elem.checked = !elem.checked;
+// });
+
+// 278
+
+// let radios = document.querySelectorAll('input[type="radio"]');
+// let button = document.querySelector('#button');
+// let p = document.querySelector('p')
+
+// button.addEventListener('click', function() {
+// 	for (let radio of radios) {
+// 		if (radio.checked) {
+// 			p.textContent = radio.value
+// 		}
+// 	}
+// });
+
+// 279
+
+// let inp = document.querySelector('input');
+// let p = document.querySelector('p')
+//  inp.addEventListener('change', function(){
+//     p.textContent = inp.value
+//  })
