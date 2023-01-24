1c0e11ccfb9cf6ad475e57414e759648c00c426e
sosopyl <122552117+sosopyl@users.noreply.github.com>
2023-01-24 09:30:27 +0300

    Update forma.js

--- forma.js
+++ forma.js
@@ -88,3 +88,51 @@
 //  inp.addEventListener('change', function(){
 //     p.textContent = inp.value
 //  })
+
+///280
+///1
+// let elem = document.querySelector('#elem');
+
+// elem.addEventListener('input', function() {
+// 	if(elem.value.length == 5){console.log(5)}
+// });
+
+//2
+// let elem = document.querySelector('#elem');
+// let p = document.querySelector('p')
+// elem.addEventListener('input', function() {
+// 	if(elem.value.length == 5){console.log(5)} 
+//    p.textContent = 5 - elem.value.length 
+// });
+
+//281
+// let elem1   = document.querySelector('#elem1');
+// let elem2   = document.querySelector('#elem2');
+
+// elem1.addEventListener('input', function() {
+// 	if(elem1.value.length == 2){elem2.focus()}
+// });
+// elem2.addEventListener('input', function() {
+// 	if(elem2.value.length == 2){elem2.blur()}
+// });
+
+//282
+
+//1
+
+// let select = document.querySelector('#select');
+// let p = document.querySelector('p')
+// let btn = document.querySelector('#btn')
+// btn.addEventListener('click', function(){
+// 	p.textContent = select.value
+// });
+
+//2
+
+// let select = document.querySelector('#select');
+// let p = document.querySelector('p')
+
+// select.addEventListener('change', function(){
+//     if( select.value % 4 == 0 ){	p.textContent = 'високосный'} else {p.textContent = ' не високосный'}
+// });
+
